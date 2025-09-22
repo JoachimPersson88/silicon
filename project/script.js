5d14c472addecbccba114df6b616c14519f1d41d
JoachimPersson88 <joachim.persson88@gmail.com>
2025-09-22 22:07:55 +0200

    added button switch to darkmode and also constructed the logos in darkmode toggle.

--- project/script.js
+++ project/script.js
@@ -3,5 +3,8 @@ const toggleButton = document.getElementById('darkModeToggle');
 
 // När man klickar på knappen togglar vi klassen på <body>
 toggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('darkmode');
+  // Toggla dark mode-klassen på body
+  document.body.classList.toggle('darkMode');
+  // Toggla "is-on"-klassen på själva knappen för animation
+  toggleButton.classList.toggle('is-on');   
 });
